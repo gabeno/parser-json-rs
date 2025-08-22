f3b1ff1ca00bd6c368aee5a63a34cbca3373c815
Gabe <gmajivu@gmail.com>
2025-08-22 17:13:48 +0300

    define json data

---
+++ src/lib.rs
@@ -0,0 +1,22 @@
+use std::collections::HashMap;
+
+/// Representation of a JSON [value](https://www.rfc-editor.org/rfc/rfc8259#section-3)
+enum Value {
+    /// literal characters `null`
+    Null,
+
+    /// literal characters `true` or `false`
+    Boolean(bool),
+
+    /// characters within double quotes "..."
+    String(String),
+
+    /// numbers stored as 64-bit floating point
+    Number(f64),
+
+    /// Zero to many JSON values
+    Array(Vec<Value>),
+
+    /// String keys with JSON values
+    Object(HashMap<String, Value>),
+}
