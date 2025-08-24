cf583a0cdc72140632f47323066edbe207f0eba7
Gabe <gmajivu@gmail.com>
2025-08-24 18:14:07 +0300

    tokenize punctuation characters

--- src/tokenize.rs
+++ src/tokenize.rs
@@ -1,5 +1,6 @@
 // REference for possible tokens https://www.json.org/json-en.html
 
+#[derive(Debug, PartialEq)]
 pub enum Token {
     // punctuation tokens
     /// `{`
@@ -29,5 +30,51 @@ pub enum Token {
 }
 
 pub fn tokenize(input: String) -> Vec<Token> {
-    Vec::new()
+    let chars: Vec<char> = input.chars().collect();
+    let mut index = 0;
+    let mut tokens: Vec<Token> = Vec::new();
+
+    while index < chars.len() {
+        let token = make_token(chars[index]);
+        tokens.push(token);
+        index += 1
+    }
+
+    tokens
+}
+
+fn make_token(ch: char) -> Token {
+    match ch {
+        '{' => Token::LeftCurlyBracket,
+        '}' => Token::RightCurlyBracket,
+        '[' => Token::LeftSquareBracket,
+        ']' => Token::RightSquareBracket,
+        ':' => Token::Colon,
+        ',' => Token::Comma,
+
+        // others
+        _ => todo!("implement other tokens"),
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::{Token, tokenize};
+
+    #[test]
+    fn test_tokenize_comma() {
+        let comma = String::from(",{}[]:");
+        let expected = vec![
+            Token::Comma,
+            Token::LeftCurlyBracket,
+            Token::RightCurlyBracket,
+            Token::LeftSquareBracket,
+            Token::RightSquareBracket,
+            Token::Colon,
+        ];
+
+        let actual = tokenize(comma);
+
+        assert_eq!(expected, actual);
+    }
 }
